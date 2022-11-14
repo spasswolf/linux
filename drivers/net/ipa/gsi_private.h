21ef2181f7136b6e2ccb21a5cf9b47e0c8c20644
Yassine Oudjana <y.oudjana@protonmail.com>
2022-11-14 10:07:54 +0300

    net: ipa: Move GSI-specific functions to an ops struct
    Add a gsi_ops struct and add all GSI-specific functions to it, then
    use it to call those functions. This will make it easy to plug-in
    a new BAM backend for use with IPA 2.* that will be added in the future.
    In order to reuse gsi_channel_tre_max for the BAM backend, don't add it
    to the ops struct but move its template to gsi_private.h for now.
    
    This makes no functional change.
    
    Signed-off-by: Yassine Oudjana <y.oudjana@protonmail.com>

--- drivers/net/ipa/gsi_private.h
+++ drivers/net/ipa/gsi_private.h
@@ -37,6 +37,15 @@ void gsi_trans_move_polled(struct gsi_trans *trans);
 void gsi_trans_complete(struct gsi_trans *trans);
 
 /**
+ * gsi_channel_tre_max() - Channel maximum number of in-flight TREs
+ * @gsi:	GSI pointer
+ * @channel_id:	Channel whose limit is to be returned
+ *
+ * Return:	 The maximum number of TREs outstanding on the channel
+ */
+u32 gsi_channel_tre_max(struct gsi *gsi, u32 channel_id);
+
+/**
  * gsi_channel_trans_mapped() - Return a transaction mapped to a TRE index
  * @channel:	Channel associated with the transaction
  * @index:	Index of the TRE having a transaction
@@ -84,47 +93,5 @@ int gsi_channel_trans_init(struct gsi *gsi, u32 channel_id);
  * @channel:	Channel whose transaction information is to be cleaned up
  */
 void gsi_channel_trans_exit(struct gsi_channel *channel);
-
-/**
- * gsi_channel_doorbell() - Ring a channel's doorbell
- * @channel:	Channel whose doorbell should be rung
- *
- * Rings a channel's doorbell to inform the GSI hardware that new
- * transactions (TREs, really) are available for it to process.
- */
-void gsi_channel_doorbell(struct gsi_channel *channel);
-
-/* gsi_channel_update() - Update knowledge of channel hardware state
- * @channel:	Channel to be updated
- *
- * Consult hardware, change the state of any newly-completed transactions
- * on a channel.
- */
-void gsi_channel_update(struct gsi_channel *channel);
-
-/**
- * gsi_ring_virt() - Return virtual address for a ring entry
- * @ring:	Ring whose address is to be translated
- * @index:	Index (slot number) of entry
- */
-void *gsi_ring_virt(struct gsi_ring *ring, u32 index);
-
-/**
- * gsi_trans_tx_committed() - Record bytes committed for transmit
- * @trans:	TX endpoint transaction being committed
- *
- * Report that a TX transaction has been committed.  It updates some
- * statistics used to manage transmit rates.
- */
-void gsi_trans_tx_committed(struct gsi_trans *trans);
-
-/**
- * gsi_trans_tx_queued() - Report a queued TX channel transaction
- * @trans:	Transaction being passed to hardware
- *
- * Report to the network stack that a TX transaction is being supplied
- * to the hardware.
- */
-void gsi_trans_tx_queued(struct gsi_trans *trans);
 
 #endif /* _GSI_PRIVATE_H_ */
