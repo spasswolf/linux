0f282acc78a286783bfa9e7f9a88239e6e9604d9
Bert Karwatzki <spasswolf@web.de>
2024-01-23 18:23:55 +0100

    drivers: net: ipa_new_legacy: remove unused IPA versions
    The removed versions were commented out before so this should be fine.
    
    Signed-off-by: Bert Karwatzki <spasswolf@web.de>

--- drivers/net/ipa-new-legacy/ipa_version.h
+++ drivers/net/ipa-new-legacy/ipa_version.h
@@ -11,20 +11,6 @@
  * @IPA_VERSION_2_0:	IPA version 2.0/BAM
  * @IPA_VERSION_2_5:	IPA version 2.5/BAM
  * @IPA_VERSION_2_6L:	IPA version 2.6 lite/BAM
- * @IPA_VERSION_3_0:	IPA version 3.0/GSI version 1.0
- * @IPA_VERSION_3_1:	IPA version 3.1/GSI version 1.0
- * @IPA_VERSION_3_5:	IPA version 3.5/GSI version 1.2
- * @IPA_VERSION_3_5_1:	IPA version 3.5.1/GSI version 1.3
- * @IPA_VERSION_4_0:	IPA version 4.0/GSI version 2.0
- * @IPA_VERSION_4_1:	IPA version 4.1/GSI version 2.0
- * @IPA_VERSION_4_2:	IPA version 4.2/GSI version 2.2
- * @IPA_VERSION_4_5:	IPA version 4.5/GSI version 2.5
- * @IPA_VERSION_4_7:	IPA version 4.7/GSI version 2.7
- * @IPA_VERSION_4_9:	IPA version 4.9/GSI version 2.9
- * @IPA_VERSION_4_11:	IPA version 4.11/GSI version 2.11 (2.1.1)
- * @IPA_VERSION_5_0:	IPA version 5.0/GSI version 3.0
- * @IPA_VERSION_5_1:	IPA version 5.1/GSI version 3.0
- * @IPA_VERSION_5_5:	IPA version 5.5/GSI version 5.5
  * @IPA_VERSION_COUNT:	Number of defined IPA versions
  *
  * Defines the version of IPA (and DMA) hardware present on the platform.
@@ -34,22 +20,6 @@ enum ipa_version {
 	IPA_VERSION_2_0,
 	IPA_VERSION_2_5,
 	IPA_VERSION_2_6L,
-#if 0
-	IPA_VERSION_3_0,
-	IPA_VERSION_3_1,
-	IPA_VERSION_3_5,
-	IPA_VERSION_3_5_1,
-	IPA_VERSION_4_0,
-	IPA_VERSION_4_1,
-	IPA_VERSION_4_2,
-	IPA_VERSION_4_5,
-	IPA_VERSION_4_7,
-	IPA_VERSION_4_9,
-	IPA_VERSION_4_11,
-	IPA_VERSION_5_0,
-	IPA_VERSION_5_1,
-	IPA_VERSION_5_5,
-#endif
 	IPA_VERSION_COUNT,			/* Last; not a version */
 };
 
